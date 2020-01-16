a7842d7d760f45a7c407a3f9944cd526b5138e8e
Deepak Nayak <39850063+deepunyk@users.noreply.github.com>
2020-01-16 15:03:50 +0530

    third

---
+++ app/src/main/java/com/xoi/sos/alertActivity.java
@@ -0,0 +1,14 @@
+package com.xoi.sos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class alertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert);
+    }
+}
--- app/src/main/java/com/xoi/sos/govMain.java
+++ app/src/main/java/com/xoi/sos/govMain.java
@@ -2,12 +2,18 @@ package com.xoi.sos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class govMain extends AppCompatActivity {
 
     Toolbar toolbar;
+    LinearLayout aLayout, pLayout, hLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,36 @@ public class govMain extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        aLayout = (LinearLayout)findViewById(R.id.alertLayout);
+        pLayout = (LinearLayout)findViewById(R.id.personnelLayout);
+        hLayout = (LinearLayout)findViewById(R.id.historyLayout);
+
+        aLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(govMain.this, alertActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        pLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(govMain.this, personnelActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        hLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(govMain.this, historyActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
